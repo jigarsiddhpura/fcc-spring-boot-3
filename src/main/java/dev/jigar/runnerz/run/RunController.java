6e85e5a7f2c2da7530fc1a3e459dce5edb38aaa0
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2024-06-26 18:10:41 +0530

    dependency injection

--- src/main/java/dev/jigar/runnerz/run/RunController.java
+++ src/main/java/dev/jigar/runnerz/run/RunController.java
@@ -1,13 +1,21 @@
 package dev.jigar.runnerz.run;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RunController {
     
-    @GetMapping("/home")
-    public String home() {
-        return "Hello guyzz!";
+    private final RunRepository runRepository;
+
+    public RunController(RunRepository runRepository) {
+        this.runRepository = runRepository;
+    }
+
+    @GetMapping("/api/runs")
+    List<Run> findAll() {
+        return runRepository.findAll();
     }
 }
